b15a7a570d754160f3be702140333725cbb9c023
Miguel B <jobmiguel83@gmail.com>
2025-10-21 12:00:21 -0300

    Commit directo desde desktop.
    Carga de los archivos básicos del proyecto inicializado en spring boot.
    Dependencias y configuración iniciales.

---
+++ polygen_research/src/main/java/cl/ubiobio/silkcorp/polygen_research/PolygenResearchApplication.java
@@ -0,0 +1,13 @@
+package cl.ubiobio.silkcorp.polygen_research;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolygenResearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolygenResearchApplication.class, args);
+	}
+
+}
---
+++ polygen_research/src/test/java/cl/ubiobio/silkcorp/polygen_research/PolygenResearchApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ubiobio.silkcorp.polygen_research;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolygenResearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
