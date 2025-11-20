458b4440deed77862186c3273c4ff51ea931e863
lotussjuice <jobmiguel83@gmail.com>
2025-11-20 18:21:29 -0300

    Cambio mati.

--- polygen_research/src/main/java/cl/ubiobio/silkcorp/polygen_research/DataBase/OpcionCampoCrf/OpcionCampoCrf.java
+++ polygen_research/src/main/java/cl/ubiobio/silkcorp/polygen_research/DataBase/OpcionCampoCrf/OpcionCampoCrf.java
@@ -1,24 +1,15 @@
 package cl.ubiobio.silkcorp.polygen_research.DataBase.OpcionCampoCrf;
 
 import cl.ubiobio.silkcorp.polygen_research.DataBase.CampoCrf.CampoCrf;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "opcion_campo_crf")
 @Getter
 @Setter
-@NoArgsConstructor
+
 public class OpcionCampoCrf {
 
     @Id
@@ -30,16 +21,18 @@ public class OpcionCampoCrf {
     private String etiqueta;
 
     @Column(name = "Orden")
-    private Integer orden;
+    private Integer orden; 
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "ID_Campo")
     private CampoCrf campoCrf;
 
+    public OpcionCampoCrf() {
+    }
+
     public OpcionCampoCrf(String etiqueta) {
         this.etiqueta = etiqueta;
     }
-
 
     public Integer getIdOpcion() {
         return idOpcion;
